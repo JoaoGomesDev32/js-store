1f4a5124b8bc621e5064a5e60fb793217caaa1ba
João Silva <118225101+JoaoGomesDev32@users.noreply.github.com>
2025-06-08 16:47:33 +0100

    :rocket: intruduce try/catch in api

--- src/middlewares/global.middlewares.js
+++ src/middlewares/global.middlewares.js
@@ -2,27 +2,35 @@ import mongoose from "mongoose";
 import userService from "../services/user.service.js";
 
 const validId = (req, res, next) => {
-  const id = req.params.id;
+  try {
+    const id = req.params.id;
 
-  if (!mongoose.Types.ObjectId.isValid(id)) {
-    return res.status(400).send({ message: "Invalid user ID" });
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+      return res.status(400).send({ message: "Invalid user ID" });
+    }
+
+    next();
+  } catch (err) {
+    res.status(500).send({ message: err.message });
   }
-
-  next();
 };
 
 const validUser = async (req, res, next) => {
-  const id = req.params.id;
-  const user = await userService.findByIdService(id);
+  try {
+    const id = req.params.id;
+    const user = await userService.findByIdService(id);
 
-  if (!user) {
-    return res.status(404).send({ message: "User not found" });
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    req.id = id;
+    req.user = user;
+
+    next();
+  } catch (err) {
+    res.status(500).send({ message: err.message });
   }
-
-  req.id = id;
-  req.user = user;
-
-  next();
 };
 
 export { validId, validUser };
